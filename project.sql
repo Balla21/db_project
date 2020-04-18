f5c20de5a8c10fc8b9c0473ced473ec5bb4edb36
Balla21 <42125637+Balla21@users.noreply.github.com>
2020-04-18 16:08:13 -0500

    Add files via upload

--- project.sql
+++ project.sql
@@ -15,16 +15,20 @@ create table myclientsession (
 );
 */
   
- 
+-- drop all tables
 drop table project_user cascade constraints;
+drop table course cascade constraints;
+drop table crse_section cascade constraints;
+drop table enroll cascade constraints;
 
+-- user and student table
 CREATE TABLE project_user (
-    id varchar2(15) primary key,
+    id varchar2(15) ,
     password varchar2(15),
     user_type numeric(1),
     student_user_type numeric(1),
     admin_user_type numeric(1),
-    user_stud_id varchar2(8),
+    user_stud_id varchar2(8) primary key,
     user_stud_lname varchar2(10),
     user_stud_fname varchar2(20),
     user_stud_age numeric(2),
@@ -35,12 +39,66 @@ CREATE TABLE project_user (
     user_stud_type numeric(1), --undergraduate(1)/graduate(2)
     user_stud_gpa numeric(3,2),
     user_stud_probation numeric(1) --no probation(0)/probation(1)
-   );
+  );
+
+-- course table
+  create table course(
+    crse_numb varchar2(8) primary key,
+    title varchar2(50) not null,
+    description varchar2(100),
+    credit_hrs numeric(1) not null
+  );
+
+-- section table
+  create table crse_section(
+    sect_id varchar2(10) primary key,
+    sect_time varchar2(20),
+    sect_semester varchar2(15),
+    sect_num_student numeric(2),
+    sect_max_stud numeric(2),
+    sect_crse_numb varchar2(8) references course
+  );
+
+--enrollment table
+create table enroll(
+  enr_stud_id varchar2(8),
+  enr_sect_id varchar2(10),
+  enr_grade varchar2(1),
+  primary key (enr_stud_id,enr_sect_id),
+  foreign key (enr_stud_id) references project_user(user_stud_id),
+  foreign key (enr_sect_id) references crse_section(sect_id)
+);
 
 
 --insertion of user student type
 insert into project_user (id,password,user_type,student_user_type,admin_user_type,user_stud_id,user_stud_lname,user_stud_fname,user_stud_age,user_stud_address,user_stud_city,user_stud_state,user_stud_zipcode,user_stud_type,user_stud_gpa,user_stud_probation) 
 values ('toto','toto123',1,1,0,'tj123456','toto','jack',18,'101 uco park','edmond','oklahoma','73034',1,3.5,0);
+
+-- insertion of courses
+-- database
+insert into course (crse_numb,title,description,credit_hrs)
+values ('cmsc4003','database management','learning about the fundamentals of database',3);
+
+-- linear algebra
+insert into course (crse_numb,title,description,credit_hrs)
+values ('math3143','linear algebra','fundamentals of matrices',3);
+
+
+
+
+
+-- insertion of section
+insert into crse_section (sect_id,sect_time,sect_semester,sect_num_student,sect_max_stud,sect_crse_numb)
+values ('22090','4:15-5:30pm','Spring',1,10,'cmsc4003');
+
+insert into crse_section (sect_id,sect_time,sect_semester,sect_num_student,sect_max_stud,sect_crse_numb)
+values ('22264','12:00-1:15pm','Spring',1,10,'math3143');
+
+-- insertion into enroll
+insert into enroll(enr_stud_id,enr_sect_id,enr_grade) values ('tj123456','22090','B');
+insert into enroll(enr_stud_id,enr_sect_id,enr_grade) values ('tj123456','22264','A');
+
+
 
 commit;
 
--- student_user.php
+++ student_user.php
@@ -9,24 +9,32 @@
     }
 
     $sql = "select * from project_user where id='$user'";
+
     try{       
         // sql query 
         $result = oci_parse($connection, $sql);
         oci_execute($result);
     }
-
     catch(Exception $error){
         echo "Query failed to execute";
     }
-
     $row = oci_fetch_assoc($result);
-    var_dump($row);
+    //var_dump($row);
 ?>
+
+
 
 <html>
     <head>
         <title>Student User page</title> 
+
+        <style>
+            table, th, td {
+             border: 1px solid black;
+            }
+        </style>
     </head>
+
     <body>
         <h3><u> Student Page : <?php echo $user; ?></u></h3>
         <form method="post" action="logout.php">
@@ -35,7 +43,7 @@
 
        <p> Change <a href = " <?php echo 'password.php?id='. $user ; ?>  ">password</a></p>
         
-        <!-- display student information -->
+     <!-- display student information -->
        <p>student id : <?php echo $row["USER_STUD_ID"]; ?> </p>
        <p> <b>Name :</b> <?php echo $row["USER_STUD_LNAME"] .', '.$row["USER_STUD_FNAME"]  ; ?> </p>
        <p> <b>Age :</b> <?php echo $row["USER_STUD_AGE"]; ?> </p>
@@ -52,6 +60,101 @@
                 else if($row["USER_STUD_PROBATION"] == 1) echo "Probation";
             ?>
        </p>
+
+
+
+    <!-- display section taken by student -->   
+        
+       <h3><u> Student Section Information </u></h3>
+       <?php
+            $student_id = $row["USER_STUD_ID"];
+            
+            try{
+                // connection to the server
+                $connection = oci_connect ("gq047", "pkefhu", "gqiannew2:1521/pdborcl");      
+            }catch(Exception $error){
+                echo "cannot connect to the database";
+                die();
+            }
+
+            $sql_stud_section = "select * from enroll";
+            $sql_stud_section .= " join project_user on project_user.user_stud_id  = enroll.enr_stud_id";
+            $sql_stud_section .= " join crse_section on crse_section.sect_id = enroll.enr_sect_id";
+            $sql_stud_section .= " join course on course.crse_numb = crse_section.sect_crse_numb";
+            $sql_stud_section .= " where enr_stud_id = '$student_id' "; 
+
+            try{       
+                // sql query 
+                $result_stud_section = oci_parse($connection, $sql_stud_section);
+                oci_execute($result_stud_section);
+            }
+            catch(Exception $error){
+                echo "Query failed to execute";
+            }
+        
+            
+       ?>
        
+       <table>  
+            <tr>
+                <td>section</td>
+                <td>course #</td>
+                <td>title </td>
+                <td>Semester</td>
+                <td>credits #</td>
+                <td>Grade</td>
+            </tr>
+
+                  
+            <?php
+                $sum_course_credit = 0;
+                $grade = 0;
+                $total_courses = 0;
+                $total_hours = 0;
+                while ( ($row_stud_sect = oci_fetch_assoc($result_stud_section) ) != false)  {
+                    $total_courses += 1;
+                    $total_hours += intval($row_stud_sect["CREDIT_HRS"]);
+                    if ( strtolower($row_stud_sect["ENR_GRADE"])  == 'a' ){
+                        $grade = 4.0;
+                    }
+                    else if ( strtolower($row_stud_sect["ENR_GRADE"])  == 'b' ){
+                        $grade = 3.0;
+                    }
+                    else if ( strtolower($row_stud_sect["ENR_GRADE"])  == 'c' ){
+                        $grade = 2.0;
+                    }
+                    else if ( strtolower($row_stud_sect["ENR_GRADE"])  == 'b' ){
+                        $grade = 1.0;
+                    }
+                    else{
+                        $grade = 0.0;
+                    }
+                    
+                    $sum_course_credit += ($grade * intval($row_stud_sect["CREDIT_HRS"]) );
+
+            ?>
+
+            <tr>
+                <td><?php echo $row_stud_sect["ENR_SECT_ID"]; ?> </td>
+                <td><?php echo $row_stud_sect["SECT_CRSE_NUMB"]; ?> </td>
+                <td><?php echo $row_stud_sect["TITLE"]; ?> </td>
+                <td><?php echo $row_stud_sect["SECT_SEMESTER"]; ?> </td>
+                <td><?php echo $row_stud_sect["CREDIT_HRS"]; ?> </td>
+                <td><?php echo $row_stud_sect["ENR_GRADE"]; ?> </td>
+
+            </tr>
+                    
+
+            <?php } ?> 
+       </table>
+
+       <p> total number of courses completed: <?php echo $total_courses; ?>  </p>
+       <p> total number of hours : <?php echo $total_hours; ?>  </p>
+       <p> Average Gpa : <?php
+            $gpa = $sum_course_credit / $total_hours;
+            echo  $gpa; ?>  
+       </p>
+
+
     </body>
 </html>
